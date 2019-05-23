09189eea1ebb48908916b89535b26a4d82f6e62f
Dan <dan.zajac@mac.com>
2019-05-23 21:25:56 +0000

    app.js edit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,3 +8,34 @@ $(() => {
     }
   });;
 });
+
+
+function handleFormSubmit() {
+  const searchbutton = $('#submitform');
+  searchbutton.on("submit", function(event) {
+      event.preventDefault();
+      let input = $('#textarea').val();
+      let api = `http://www.omdbapi.com/?t=${input}&apikey=d566210c`;
+      fetch(api)
+      .then((resp) => resp.json())
+      .then(function(movie) {
+          $("#movielist").append(`<li>${movie.Title} - ${movie.Type}</li>`)
+      });  
+  })
+  .catch(function(err) {
+    alert(err);
+  }) 
+}
+
+$(document).ready(function(){
+  handleFormSubmit();
+  homePage();
+})
+
+
+
+
+app.listen(PORT, () => {
+  console.log("Example app listening on port " + PORT);
+});
+
