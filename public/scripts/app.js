07891cd102029b244348f87babccd706ac98e5c5
Dan <dan.zajac@mac.com>
2019-05-24 02:35:55 +0000

    multiple apis added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,10 +11,10 @@
 //   });;
 // });
 
-
-function handleFormSubmit() {
-  const searchbutton = $('#submitform');
-  searchbutton.on("submit", function(event) {
+//Movie GET
+function movieFormSubmit() {
+  const movieButton = $('#submitform');
+  movieButton.on("submit", function(event) {
       event.preventDefault();
       let input = $('#textarea').val();
       let api = `http://www.omdbapi.com/?t=${input}&apikey=d566210c`;
@@ -29,8 +29,44 @@ function handleFormSubmit() {
   }) 
 }
 
+//Book GET ---- Change Jquery id tags for Separate button
+function bookFormSubmit() {
+  const bookButton = $('#submitform');
+  bookButton.on("submit", function(event) {
+      event.preventDefault();
+      let input = $('#textarea').val();
+      let api = `https://www.googleapis.com/books/v1/volumes?q=${input}&key=AIzaSyDd1bZjix5lQXjGFAZ8x67mblYDt7VtxoA`;
+      fetch(api)
+      .then((resp) => resp.json())
+      .then(function(book) {
+          $("#movielist").append(`<li>${book.items[0].volumeInfo.title} - ${book.items[0].volumeInfo.printType}</li>`)
+      });  
+  })
+  .catch(function(err) {
+    alert(err);
+  }) 
+}
+
+function restoFormSubmit() {
+  const restoButton = $('#submitform');
+  restoButton.on("submit", function(event) {
+      event.preventDefault();
+      let input = $('#textarea').val();
+      let api = `https://developers.zomato.com/api/v2.1/search?entity_id=300&entity_type=city&q=${input}`;
+      fetch(api)
+      .then((resp) => resp.json())
+      .then(function(resto) {
+          $("#movielist").append(`<li>${resto.restaurants[0].restaurant.name}</li>`)
+      });  
+  })
+  .catch(function(err) {
+    alert(err);
+  }) 
+}
+
 $(document).ready(function(){
-  handleFormSubmit();
+  // movieFormSubmit()
+  restoFormSubmit();
   homePage();
 })
 
@@ -47,4 +83,18 @@ app.listen(PORT, () => {
 // 017873622156579969207:tayz2vdgawe
 
 
-// AIzaSyDd1bZjix5lQXjGFAZ8x67mblYDt7VtxoA
+//
+
+//Google Books
+// https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
+
+
+//GOOGLE API
+//AIzaSyDd1bZjix5lQXjGFAZ8x67mblYDt7VtxoA
+
+
+//Zomato API Key
+// 7fc0ba52b112ebf563c06ad6256d9b8e
+
+//Zomato Restos
+// https://developers.zomato.com/api/v2.1/search?entity_id=300&entity_type=city&q=Cactus
