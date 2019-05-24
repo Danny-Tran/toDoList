bc0a8bd08944b7192384dd0f2189de365d0ebc79
Kate <khummle@gmail.com>
2019-05-24 18:13:47 +0000

    reactive design

--- public/scripts/homePage.js
+++ public/scripts/homePage.js
@@ -2,12 +2,11 @@ function homePage(){
     $('#page').html(
         `
         <section class="addCheckIt">
-            <h1>What do you want to Check Out?</h1>
+
             <form id="addToDo" action="/" method="POST">
                 <div class="input-group mt-3 mb-3">
                     <div class="input-group-prepend">
-                        <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown">
-                        What are you looking for?
+                    <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown">
                         </button>
                         <div class="dropdown-menu">
                         <a class="dropdown-item" href="#">Watch</a>
@@ -16,9 +15,9 @@ function homePage(){
                         <a class="dropdown-item" href="#">Buy</a>
                         <a class="dropdown-item" href="#">Misc</a>
                     </div>
+                    <textarea placeholder="Add something to Check Out!"
+                        name="text"></textarea>
                 </div>
-                <textarea placeholder="Add something to Check Out!"
-                    name="text"></textarea>
                 <div class="addCheckItFooter">
                     <input class="checkIt" type="submit" 
                         value="checkIt">
