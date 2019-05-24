d03b54b7459a5ce81779e0aea4ea553042a0a375
Dan <dan.zajac@mac.com>
2019-05-24 21:20:53 +0000

    added Section to index.html

--- public/scripts/homePage.js
+++ public/scripts/homePage.js
@@ -1,7 +1,6 @@
 function homePage(){
     $('#page').html(
         `
-<<<<<<< HEAD
         <section class="addCheckIt" id="parent-section">
             <h1>What do you want to Check Out?</h1>
             <form id="addToDo" action="/" method="POST">
@@ -17,25 +16,6 @@ function homePage(){
                 <textarea placeholder="Add something to Check Out!" name="text">
                 </textarea>
                 <input class="checkIt" type="submit" value="checkIt">
-=======
-        <section class="addCheckIt">
-
-            <form id="addToDo" action="/" method="POST">
-                <div class="input-group mt-3 mb-3">
-                    <div class="input-group-prepend">
-                    <button type="button" class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown">
-                        </button>
-                        <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Watch</a>
-                        <a class="dropdown-item" href="#">Eat</a>
-                        <a class="dropdown-item" href="#">Read</a>
-                        <a class="dropdown-item" href="#">Buy</a>
-                        <a class="dropdown-item" href="#">Misc</a>
-                    </div>
-                    <textarea placeholder="Add something to Check Out!"
-                        name="text"></textarea>
-                </div>
->>>>>>> master
                 <div class="addCheckItFooter">
                     
                 </div>
